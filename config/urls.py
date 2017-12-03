83e0de08f195e21f0fc3254e2c3eef0ece73b47e
Jeff Triplett <jeff.triplett@gmail.com>
2017-12-03 00:42:38 -0600

    :pencil: Adds home, about, and common nav links
    Signed-off-by: Jeff Triplett <jeff.triplett@gmail.com>

--- config/urls.py
+++ config/urls.py
@@ -2,13 +2,15 @@ from django.conf import settings
 from django.conf.urls import include, url
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 from organizations.backends import invitation_backend, registration_backend
 
 
 urlpatterns = [
     # our applications
-    url(r'^proposals/', include('proposals.urls', namespace='proposals')),
+    url(r'^$', TemplateView.as_view(template_name='homepage.html'), name='homepage'),
+    url(r'^about/$', TemplateView.as_view(template_name='about.html'), name='about'),
 
     # third-party applications
     url(r'^accounts/', include('allauth.urls')),
